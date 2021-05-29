c266b00d12b9dc17cdfa2c6175d2c1be16cfc9ff
domonkos11 <domonkos11@sulid.hu>
2021-05-29 18:08:08 +0200

    Uptime added

--- index.js
+++ index.js
@@ -44,24 +44,32 @@ bot.on('message', (message) => {
 
     const parts = message.content.split(' ');
     if(parts[0] === '!help-levels'){
-        message.channel.send(new Discord.MessageEmbed().setTitle("Help").setDescription("**Hey! You said that you want the commands, right? OK, here you are: ** \n  1. !rank-levels : view your current rank. \n  2. !testlevels-levels : test if the bot working or not. \n  3. !botinfo-levels : how many servers does the bot in? \n  4. !help-levels : this help. \n  5. !uptime-levels : View the bot's current uptime. \n  6. !ping-levels : View the bot's ping.").setColor("#3277a8"))
-        message.channel.send("**INFO: The bot is in " + message.client.guilds.cache.size + " servers. Once it reaches 100, you can't use it anymore. Take it, or leave it :) So send it to your friends, and they can use the bot before we reach 100 servers!  **")
+        message.channel.send(new Discord.MessageEmbed().setTitle("Help").setDescription("**Hey! You said that you want the commands, right? OK, here you are: ** \n  1. !rank-levels : view your current rank. \n  2. !testlevels-levels : test if the bot working or not. \n  3. !botinfo-levels : how many servers does the bot in? \n  4. !help-levels : this help. \n  5. !uptime-levels : View the bot's current uptime. \n  6. !ping-levels : View the bot's ping.").setColor("#3277a8"));
+        message.channel.send("**INFO: The bot is in " + message.client.guilds.cache.size + " servers. Once it reaches 100, you can't use it anymore. Take it, or leave it :) So send it to your friends, and they can use the bot before we reach 100 servers!  **");
     }
     if(parts[0] === '!testlevels-levels') {
         message.reply('levels are working');
-        message.channel.send("**INFO: The bot is in " + message.client.guilds.cache.size + " servers. Once it reaches 100, you can't use it anymore. Take it, or leave it :) So send it to your friends, and they can use the bot before we reach 100 servers!  **")
+        message.channel.send("**INFO: The bot is in " + message.client.guilds.cache.size + " servers. Once it reaches 100, you can't use it anymore. Take it, or leave it :) So send it to your friends, and they can use the bot before we reach 100 servers!  **");
     }
     if(parts[0] === '!botinfo-levels'){
-        message.reply("The bot is currently in " + message.client.guilds.cache.size + " servers. Hopefully it goes bigger! :)")
+        message.reply("The bot is currently in " + message.client.guilds.cache.size + " servers. Hopefully it goes bigger! :)");
     }
     if(parts[0] === '!rank-levels'){
-        message.reply("You are currently on level " + userStats.level + ". Good luck! :)")
+        message.reply("You are currently on level " + userStats.level + ". Good luck! :)");
     }
     if(parts[0] === '!uptime-levels'){
-        message.reply("I have been up for like " + "currently working on it, shh") //bot.uptime displays in ms, I don't know how to do it
+        let totalSeconds = (client.uptime / 1000);
+        let days = Math.floor(totalSeconds / 86400);
+        totalSeconds %= 86400;
+        let hours = Math.floor(totalSeconds / 3600);
+        totalSeconds %= 3600;
+        let minutes = Math.floor(totalSeconds / 60);
+        let seconds = Math.floor(totalSeconds % 60);
+        let uptime = `${days} days, \n ${hours} hours, \n ${minutes} minutes, and \n ${seconds} seconds`;
+        message.reply("I have been up for like \n" + uptime);
     }
     if(parts[0] === '!ping-levels'){
-        message.reply("Pong!" + " And the ping is: " + bot.ws.ping)
+        message.reply("Pong!" + " And the ping is: " + bot.ws.ping);
     }
 });
 
